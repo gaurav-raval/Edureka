df70e7e866a77d10f71cb1bf33e89845d01619ee
Gaurav Raval <115720781+gaurav-raval@users.noreply.github.com>
2025-02-05 21:22:30 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
 
       {/* Sidebar */}
       <div className={`fixed top-0 left-0 h-full bg-gray-800 text-white w-64 p-4 transform ${isOpen ? "translate-x-0" : "-translate-x-full"} md:translate-x-0 transition-transform`}>
-        <h2 className="text-xl font-bold p-4 border-b border-gray-700">Admin Panel</h2>
+        <h2 className="text-xl font-bold p-4 border-b border-gray-700">Actify</h2>
         <ul className="p-4 space-y-4">
           <li><Link to="/" className="block px-4 py-2 hover:bg-gray-700">Table</Link></li>
           <li><Link to="/Form" className="block px-4 py-2 hover:bg-gray-700">Add Profile</Link></li>
